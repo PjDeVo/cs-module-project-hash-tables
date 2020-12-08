b9425824f3504f8160c76a183f0f3e5c5387d34b
patrick devincentis <patrickdevincentis@MacBook-Pro.local>
2020-12-07 21:58:21 -0500

    nodups complete

--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,6 +1,22 @@
 def no_dups(s):
     # Your code here
 
+    #seperate the words
+    # add them to a dictionary
+    # if word is already in the dictionary, remove it from new variable
+    # return new variable
+
+
+    words = s.split()
+    newSentence = {}
+    for word in words:
+        if word in newSentence:
+            pass
+        else:
+            newSentence[word] = 1
+    return " ".join(newSentence.keys())
+
+
 
 
 if __name__ == "__main__":
